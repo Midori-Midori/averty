68bd41537ed6a23ce36569d6372e934307ea273e
Midori-Midori <mj.aguilar2003@gmail.com>
2026-06-16 16:24:17 -0700

    feat: add swiper carousel to safety cards on mobile

--- src/app/pages/home/safety/safety.component.ts
+++ src/app/pages/home/safety/safety.component.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, signal, AfterViewInit, OnDestroy, ElementRef, inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { CardComponent } from '../../../shared/card/card.component';
 import { RevealDirective } from '../../../shared/directives/reveal.directive';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
 
 interface SafetyFact {
   highlight: string;
@@ -18,7 +20,11 @@ interface SafetyFact {
   templateUrl: './safety.component.html',
   styleUrl: './safety.component.scss'
 })
-export class SafetyComponent {
+export class SafetyComponent implements AfterViewInit, OnDestroy {
+  private el = inject(ElementRef);
+  private platformId = inject(PLATFORM_ID);
+  private swiperInstance: Swiper | null = null;
+
   facts = signal<SafetyFact[]>([
     {
       highlight: '11,288',
@@ -49,4 +55,59 @@ export class SafetyComponent {
       colorClass: 'color-blue'
     }
   ]);
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.initSwiperIfNeeded();
+      window.addEventListener('resize', this.onResize);
+    }
+  }
+
+  ngOnDestroy() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.removeEventListener('resize', this.onResize);
+      this.destroySwiper();
+    }
+  }
+
+  private onResize = () => {
+    this.initSwiperIfNeeded();
+  };
+
+  private initSwiperIfNeeded() {
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    const isMobile = window.innerWidth < 768;
+    if (isMobile) {
+      if (!this.swiperInstance) {
+        const container = this.el.nativeElement.querySelector('.swiper');
+        if (container) {
+          // Wait for GSAP reveal animations to finish before initializing Swiper
+          setTimeout(() => {
+            this.swiperInstance = new Swiper(container, {
+              modules: [Pagination],
+              slidesPerView: 1,
+              centeredSlides: true,
+              spaceBetween: 30,
+              initialSlide: 0,
+              grabCursor: true,
+              pagination: {
+                el: container.querySelector('.swiper-pagination'),
+                clickable: true,
+              }
+            });
+          }, 300);
+        }
+      }
+    } else {
+      this.destroySwiper();
+    }
+  }
+
+  private destroySwiper() {
+    if (this.swiperInstance) {
+      this.swiperInstance.destroy(true, true);
+      this.swiperInstance = null;
+    }
+  }
 }
