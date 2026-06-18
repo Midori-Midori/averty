8c2d916e4599976f53b35fd1cc5507006413f531
Midori-Midori <mj.aguilar2003@gmail.com>
2026-06-18 16:29:34 -0700

    feat(stats): increase desktop card width to 1200px and improve responsive scaling

--- src/app/pages/home/stats/stats.component.ts
+++ src/app/pages/home/stats/stats.component.ts
@@ -1,10 +1,8 @@
-import { Component, signal, AfterViewInit, OnDestroy, ElementRef, inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser, CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Stat } from '../../../core/interfaces/stat.interface';
 import { RevealDirective } from '../../../shared/directives/reveal.directive';
 import { LucideAngularModule, TriangleAlert, Zap, MapPinned, Shield } from 'lucide-angular';
-import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
 
 @Component({
   selector: 'app-stats',
@@ -13,11 +11,7 @@ import { Pagination } from 'swiper/modules';
   templateUrl: './stats.component.html',
   styleUrl: './stats.component.scss'
 })
-export class StatsComponent implements AfterViewInit, OnDestroy {
-  private el = inject(ElementRef);
-  private platformId = inject(PLATFORM_ID);
-  private swiperInstance: Swiper | null = null;
-
+export class StatsComponent {
   stats = signal<Stat[]>([
     { 
       value: '43', 
@@ -57,60 +51,4 @@ export class StatsComponent implements AfterViewInit, OnDestroy {
       colorClass: 'color-green' 
     }
   ]);
-
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.initSwiperIfNeeded();
-      window.addEventListener('resize', this.onResize);
-    }
-  }
-
-  ngOnDestroy() {
-    if (isPlatformBrowser(this.platformId)) {
-      window.removeEventListener('resize', this.onResize);
-      this.destroySwiper();
-    }
-  }
-
-  private onResize = () => {
-    this.initSwiperIfNeeded();
-  };
-
-  private initSwiperIfNeeded() {
-    if (!isPlatformBrowser(this.platformId)) return;
-
-    const isMobile = window.innerWidth < 768;
-    if (isMobile) {
-      if (!this.swiperInstance) {
-        const container = this.el.nativeElement.querySelector('.swiper');
-        if (container) {
-          // Wait for GSAP reveal animations to finish before initializing Swiper
-          setTimeout(() => {
-            this.swiperInstance = new Swiper(container, {
-              modules: [Pagination],
-              slidesPerView: 1,
-              centeredSlides: true,
-              spaceBetween: 30,
-              initialSlide: 0,
-              grabCursor: true,
-              pagination: {
-                el: container.querySelector('.swiper-pagination'),
-                clickable: true,
-              }
-            });
-          }, 300);
-        }
-      }
-    } else {
-      this.destroySwiper();
-    }
-  }
-
-  private destroySwiper() {
-    if (this.swiperInstance) {
-      this.swiperInstance.destroy(true, true);
-      this.swiperInstance = null;
-    }
-  }
 }
-
